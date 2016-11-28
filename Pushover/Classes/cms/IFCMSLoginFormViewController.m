0a788d5e95cd055baaf0aaf59b17184984959849
Julian Goacher <julian.goacher@innerfunction.com>
2016-11-28 16:36:56 +0000

    Added login check to CMS login form.

--- Pushover/Classes/cms/IFCMSLoginFormViewController.m
+++ Pushover/Classes/cms/IFCMSLoginFormViewController.m
@@ -22,6 +22,8 @@
 
 @interface IFCMSLoginFormViewController ()
 
+- (IFCMSContentAuthority *)getContentAuthority;
+
 @end
 
 @implementation IFCMSLoginFormViewController
@@ -32,12 +34,10 @@
         __block IFCMSLoginFormViewController *this = self;
         IFFormView *form = self.form;
         form.onSubmit = ^(IFFormView *form, NSDictionary *values) {
-            IFContentProvider *provider = [IFContentProvider getInstance];
-            id authority = [provider contentAuthorityForName:this.authority];
-            if ([authority isKindOfClass:[IFCMSContentAuthority class]]) {
-                IFCMSContentAuthority *cmsAuthority = (IFCMSContentAuthority *)authority;
+            IFCMSContentAuthority *authority = [this getContentAuthority];
+            if (authority) {
                 [form submitting:YES];
-                [cmsAuthority loginWithCredentials:values]
+                [authority loginWithCredentials:values]
                 .then( (id)^(BOOL ok) {
                     [form submitting:NO];
                     [this postMessage:this.onlogin];
@@ -51,6 +51,25 @@
         };
     }
     return self;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    // Check whether the user is already logged in.
+    if ([[self getContentAuthority].authManager isLoggedIn]) {
+        [self postMessage:self.onlogin];
+    }
+}
+
+#pragma mark - private
+
+- (IFCMSContentAuthority *)getContentAuthority {
+    IFContentProvider *provider = [IFContentProvider getInstance];
+    id authority = [provider contentAuthorityForName:self.authority];
+    if ([authority isKindOfClass:[IFCMSContentAuthority class]]) {
+        return (IFCMSContentAuthority *)authority;
+    }
+    return nil;
 }
 
 @end
