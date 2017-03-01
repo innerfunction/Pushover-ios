5755bfa726f6fa757dd313a00e7e69afe3a5ba52
Julian Goacher <julian.goacher@innerfunction.com>
2017-03-01 15:42:53 +0000

    Added content app container.

---
+++ Smokestack/Classes/content/IFContentAppContainer.m
@@ -0,0 +1,40 @@
+// Copyright 2016 InnerFunction Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//  Created by Julian Goacher on 01/03/2017.
+//  Copyright © 2017 InnerFunction. All rights reserved.
+//
+
+#import "IFContentAppContainer.h"
+#import "IFContentSchemeHandler.h"
+#import "NSDictionary+IF.h"
+
+@implementation IFContentAppContainer
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        // Add a content: scheme handler.
+        [_appURIHandler addHandler:[IFContentSchemeHandler new] forScheme:@"content"];
+        // Modify standard configuration with content provider settings.
+        self.standardConfiguration = [self.standardConfiguration extendWith:@{
+            @"contentProvider": @{
+                @"authorities":  @"@dirmap:/SCFFLD/cauths"
+            }
+        }];
+    }
+    return self;
+}
+
+@end
